3c2a97bee50f9817048f3ad063609c3a950ccb3b
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-03 22:05:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  const submitHandler = (e)=>{
+    console.log("form submitted");
+    e.preventDefault();
+  }
+  return (
+    <div className='h-screen bg-black text-white'>
+      <form className='p-2 flex flex-wrap justify-center' onSubmit={submitHandler}>  
+        < input className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' type="text" placeholder='Enter your name'/>
+        < input className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' type="text" placeholder='Image URL'/>
+        < input className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' type="text" placeholder='Enter Role'/>
+        < input className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' type="text" placeholder='Enter Description'/>
+        <button className='bg-emerald-500 active:scale-95 border-2 px-5 py-2 rounded m-2 w-[90%]'>Create User</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
