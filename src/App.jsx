0ca2fe64fa5bcbe7ce8f04026c2d989e5a15513e
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-03 23:16:37 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -30,37 +30,43 @@ const App = () => {
          onChange={(e)=>{
           setUserName(e.target.value)
          }}
-         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
+         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[48%]' 
           type="text" placeholder='Enter your name'/>
         < input
           value={imgURL}
           onChange={(e)=>{
           setImgURL(e.target.value)
         }}
-         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
+         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[48%]' 
           type="text" placeholder='Image URL'/>
         < input 
           value={userRole}
           onChange={(e)=>{
             setUserRole(e.target.value)
           }}
-          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[48%]' 
           type="text" placeholder='Enter Role'/>
         < input
           value={userDesc}
           onChange={(e)=>{
             setUserDesc(e.target.value)
           }}
-          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[48%]' 
           type="text" placeholder='Enter Description'/>
         <button 
-          className='bg-emerald-500 active:scale-95 border-2 px-5 py-2 rounded m-2 w-[90%]'>Create User</button>
+          className='bg-emerald-500 active:scale-95  px-2 py-2 rounded m-2 w-[96%]'>Create User</button>
       </form>
 
-      <div className='px-4 py-10 flex flex-wrap '>
+      <div className='px-4 py-10 flex flex-wrap gap-4 '>
           {
-            allUsers.map(function(){
-              return <Card/>
+            allUsers.map(function(elem,idx){
+              return <div key={idx} className='w-[23vw]  bg-white text-black rounded-xl px-8 py-8 text-center flex flex-col items-center '>
+              <img className='h-24 w-24 rounded-full object-cover object-center' src={elem.imgURL} alt="" />
+              <h1 className='text-2xl mt-2 font-semibold'>{elem.userName}</h1>
+              <h5 className='text-lg text-blue-500 font-semibold my-3'>{elem.userRole}</h5>
+              <p className='text-sm font-medium leading-tight'>{elem.userDesc}</p>
+              <button className='px-4 py-2 bg-red-600 text-white text-xs cursor-pointer active:scale-95 font-semibold mt-3'>Remove</button>
+            </div>
             })
           }
       </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='w-[24vw] h-80 bg-white text-black'>
-      <img  src="https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div className='w-[23vw]  bg-white text-black rounded-xl px-8 py-8 text-center flex flex-col items-center '>
+      <img className='h-24 w-24 rounded-full' src="https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      <h1 className='text-2xl mt-2 font-semibold'>Yuvraj Krishna</h1>
+      <h5 className='text-lg text-blue-500 font-semibold my-3'>Developer</h5>
+      <p className='text-sm font-medium leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, ut.</p>
+      <button className='px-4 py-2 bg-red-600 text-white text-xs cursor-pointer active:scale-95 font-semibold mt-3'>Remove</button>
     </div>
   )
 }
