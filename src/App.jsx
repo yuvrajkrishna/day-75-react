11629ffc0ba62450434b18199c24857be235fc0f
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-02-03 22:44:51 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,71 @@
 import React, { useState } from 'react'
-
+import Card from './Components/Card'
 const App = () => {
   const [userName,setUserName] = useState('')
+  const [userRole,setUserRole] = useState('')
+  const [imgURL,setImgURL] = useState('')
+  const [userDesc,setUserDesc] = useState('')
+
+  const [allUsers,setAllUsers] = useState([])
+
   const submitHandler = (e)=>{
-    console.log("form submitted");
     e.preventDefault();
+
+    const oldUsers = [...allUsers]
+    oldUsers.push({userName,userRole,userDesc,imgURL})
+    console.log("form submitted");
+    // console.log(userName)
+    console.log(oldUsers)
+    setAllUsers(oldUsers)
+    setUserName('')
+    setUserRole('')
+    setImgURL('')
+    setUserDesc('')
   }
   return (
     <div className='h-screen bg-black text-white'>
-      <form className='p-2 flex flex-wrap justify-center' onSubmit={submitHandler}>  
+      <form className='p-2 flex flex-wrap ' onSubmit={submitHandler}>  
         < input
-         className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' 
+         value={userName}
+         onChange={(e)=>{
+          setUserName(e.target.value)
+         }}
+         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
           type="text" placeholder='Enter your name'/>
         < input
-         className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' 
+          value={imgURL}
+          onChange={(e)=>{
+          setImgURL(e.target.value)
+        }}
+         className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
           type="text" placeholder='Image URL'/>
         < input 
-          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' 
+          value={userRole}
+          onChange={(e)=>{
+            setUserRole(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
           type="text" placeholder='Enter Role'/>
         < input
-          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 w-[45%]' 
+          value={userDesc}
+          onChange={(e)=>{
+            setUserDesc(e.target.value)
+          }}
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-1 w-[45%]' 
           type="text" placeholder='Enter Description'/>
         <button 
           className='bg-emerald-500 active:scale-95 border-2 px-5 py-2 rounded m-2 w-[90%]'>Create User</button>
       </form>
+
+      <div className='px-4 py-10 flex flex-wrap '>
+          {
+            allUsers.map(function(){
+              return <Card/>
+            })
+          }
+      </div>
     </div>
+    
   )
 }
 
---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-[24vw] h-80 bg-white text-black'>
+      <img  src="https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
+  )
+}
+
+export default Card
